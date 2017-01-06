90af281d8af22543b7fe438f05eefbec44696444
Supriya2396 <supriyavenkataswamy@gmail.com>
2017-01-06 18:30:40 +0530

    Create magicmat.c

---
+++ magicmat.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    int i,j,k,iold,jold;
+    i=0;
+    j=n/2;
+    A[i][j]=1;
+    for(int k=2;k<=n*n;k++)
+    {
+        iold=i;
+        jold=j;
+        if(i==0)
+            i=n-1;
+        else
+            i=i-1;
+        if(j==n-1)
+            j=0;
+        else
+            j=j+1;
+        while(A[i][j]!=0)
+        {
+            if(i==n-1)
+                i=0;
+            else
+                i=iold+1;
+                j=jold;
+        }
+        A[i][j]=k;
+     }
+ }
